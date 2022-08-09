d9d44e0138731e09afc0a1b802ab36257bd3e352
Jeffrey Lamar <jlam3950@gmail.com>
2022-08-09 08:26:52 -0400

    init repo

---
+++ src/redux/todoSlice.js
@@ -0,0 +1,47 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const getTodoAsync = createAsyncThunk('todos/getTodosAsync',
+async () => {
+    const response = await fetch('http://localhost:7000/todos')
+    if(response.ok){
+        const todos = await response.json();
+        return { todos }
+    }
+})
+
+const todoSlice = createSlice({
+    name: 'todos',
+    initialState: [
+        { id:1, title: 'todo1', completed: false },
+        { id:2, title: 'todo2', completed: false },
+        { id:3, title: 'todo3', completed: true },
+    ],
+    reducers: {
+        addTodo: (state,action) => {
+            const newTodo = {
+                id: Date.now(),
+                title: action.payload.title,
+                completed: false,
+            };
+            state.push(newTodo);
+        },
+        toggleComplete: (state, action) => {
+            const index = state.findIndex((todo) => todo.id === action.payload.id
+            );
+            state[index].completed = action.payload.completed;
+       },
+       deleteTodo: (state, action) => {
+            return state.filter((todo)=> todo.id !== action.payload.id);
+       }
+    },
+});
+
+export const { 
+    addTodo,
+    toggleComplete,
+    deleteTodo,  
+} = todoSlice.actions; 
+
+export default todoSlice.reducer; 
+
+//slice gives us a way to store a slice of data, and also gives us way to retreive that data 
